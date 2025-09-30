5006b337ab9c77e97045aba20e86517ebf758f80
NicollasZoratto <nzcarvalho17@gmail.com>
2025-09-30 01:29:39 +0000

    revisao poo 29/09/25

---
+++ src/main/java/application/Cachorro.java
@@ -0,0 +1,13 @@
+package application;
+
+public class Cachorro extends Animal {
+    
+    public Cachorro(String nome) {
+        super(nome);
+    }
+    
+    @Override
+    public String comunicar() {
+        return "Au, Au";
+    }
+}
--- src/main/java/application/Main.java
+++ src/main/java/application/Main.java
@@ -2,7 +2,19 @@ package application;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!!!");
+        Gato gato = new Gato("Bichano");
+
+        Animal[] animais = new Animal[4];
+        animais[0] = gato;
+        animais[1] = new Cachorro("Toto");
+        animais[2] = new Cachorro("Snoopey");
+        animais[3] = new Gato("Malhado");
+
+        for (Animal a : animais) {
+            System.out.println(a);
+        } 
+
+        System.out.println(gato.comunicar("Teste"));
     }
 }
 
